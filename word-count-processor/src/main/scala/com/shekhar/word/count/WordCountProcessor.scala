577a15f0f1bea9f035ae2c763d03359fbd46381d
shekhar-kotekar <shekhar.kotekar@gmail.com>
2020-05-19 15:49:42 +0100

    Test cases fixed and code updated.

--- word-count-processor/src/main/scala/com/shekhar/word/count/WordCountProcessor.scala
+++ word-count-processor/src/main/scala/com/shekhar/word/count/WordCountProcessor.scala
@@ -1,7 +1,138 @@
 package com.shekhar.word.count
 
+import java.io.InputStream
+import java.nio.charset.StandardCharsets
+import java.util
+
 import com.typesafe.scalalogging.LazyLogging
+import org.apache.nifi.annotation.documentation.{CapabilityDescription, Tags}
+import org.apache.nifi.components.PropertyDescriptor
+import org.apache.nifi.flowfile.FlowFile
+import org.apache.nifi.processor.io.InputStreamCallback
+import org.apache.nifi.processor.util.StandardValidators
+import org.apache.nifi.processor.{AbstractProcessor, ProcessContext, ProcessSession, ProcessorInitializationContext, Relationship}
+import org.apache.nifi.stream.io.StreamUtils
 
-class WordCountProcessor extends LazyLogging {
+import scala.collection.JavaConverters._
 
+/**
+ * As name suggests, this processor does word count operation on flow files.
+ * It extends AbstractProcessor class given by NiFi and
+ * it also extends LazyLogging trait give by typesafe logging.
+ */
+@Tags(Array("word", "count", "custom"))
+@CapabilityDescription(
+  "This processor reads contents of a flow file," +
+    " converts it to a string and splits the string on whitespace," +
+    " counts occurance of each word and finally" +
+    " emits one flow file for each word." +
+    " Each flow file has two attributes called 'word' and 'count'."
+)
+class WordCountProcessor extends AbstractProcessor with LazyLogging {
+
+  /**
+   * successful flow files will be directed to this relation
+   */
+  val SUCCESS: Relationship = new Relationship.Builder().name(WordCountProcessor.successRelationName).build
+  /**
+   * Flow files will be redirected to this relation if processor encounters problem
+   * while processing a flow file.
+   */
+  val FAILURE: Relationship = new Relationship.Builder().name(WordCountProcessor.failureRelationName).build
+  // for simplicity, we will stick to only these stop words.
+  private val stopWords: Seq[String] = Seq("of", "this", "the", "me", "myself", "his", "and", "a")
+  /**
+   * Processor will use value of this property descriptor to determine if
+   * we need to consider stop words like of, the, will be counted too or not.
+   * If value for this property is true then stop words will be counted;
+   * otherwise not.
+   */
+  var skipStopWords: PropertyDescriptor = _
+
+  /**
+   * Build the property descriptor object
+   *
+   * @param context
+   */
+  override def init(context: ProcessorInitializationContext): Unit = {
+    skipStopWords = new PropertyDescriptor.Builder()
+      .name(WordCountProcessor.skipStopWordsPropertyName)
+      .displayName("Skip stop words")
+      .description("Set to true so that processor will not count stop words like the, of, etc.")
+      .required(false)
+      .defaultValue("true")
+      .addValidator(StandardValidators.BOOLEAN_VALIDATOR)
+      .build()
+  }
+
+  override def getSupportedPropertyDescriptors: util.List[PropertyDescriptor] = {
+    Seq(skipStopWords).asJava
+  }
+
+  override def getRelationships: util.Set[Relationship] = {
+    Set(SUCCESS, FAILURE).asJava
+  }
+
+  /**
+   * We will process incoming flow file through this method.
+   *
+   * @param processContext
+   * @param processSession
+   */
+  override def onTrigger(processContext: ProcessContext
+                         , processSession: ProcessSession): Unit = {
+    val flowFile: FlowFile = processSession.get()
+    if (flowFile != null) {
+      val skipStopWords: Boolean = processContext
+        .getProperty(WordCountProcessor.skipStopWordsPropertyName)
+        .getValue
+        .toBoolean
+      process(flowFile, skipStopWords, processSession)
+    }
+  }
+
+  private def process(inputFlowFile: FlowFile
+                      , skipStopWords: Boolean
+                      , session: ProcessSession): Unit = {
+    val flowFileContents: String = this.readFlowFileContents(inputFlowFile, session)
+
+    // we want to filter out blank strings
+    // and when skipStopWords is set to true then we should filter out stop words
+    val words: Seq[String] = flowFileContents
+      .toLowerCase
+      .split(" ")
+      .filterNot(_.trim.length == 0)
+      .filterNot(word => skipStopWords && stopWords.contains(word))
+
+    val wordCounts: Map[String, Int] = words
+      .groupBy((word: String) => word)
+      .mapValues(_.length)
+
+    val outputFlowFiles: Seq[FlowFile] = wordCounts
+      .map(wordCount => {
+        val outputFlowFile: FlowFile = session.clone(inputFlowFile)
+        session.putAttribute(outputFlowFile, "word", wordCount._1)
+        session.putAttribute(outputFlowFile, "count", wordCount._2.toString)
+        outputFlowFile
+      }).toSeq
+
+    session.transfer(outputFlowFiles.asJava, SUCCESS)
+    session.remove(inputFlowFile)
+  }
+
+  private def readFlowFileContents(flowFile: FlowFile, session: ProcessSession): String = {
+    val flowFileContentsBuffer = new Array[Byte](flowFile.getSize.toInt)
+    session.read(flowFile, new InputStreamCallback {
+      override def process(inputStream: InputStream): Unit = {
+        StreamUtils.fillBuffer(inputStream, flowFileContentsBuffer, true)
+      }
+    })
+    new String(flowFileContentsBuffer, StandardCharsets.UTF_8)
+  }
+}
+
+object WordCountProcessor {
+  val skipStopWordsPropertyName: String = "skip-stop-words"
+  val successRelationName: String = "success"
+  val failureRelationName: String = "failure"
 }
--- word-count-processor/src/test/scala/com/shekhar/word/count/TestWordCountProcessor.scala
+++ word-count-processor/src/test/scala/com/shekhar/word/count/TestWordCountProcessor.scala
@@ -1,17 +1,79 @@
 package com.shekhar.word.count
 
-import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
+import org.apache.nifi.flowfile.FlowFile
+import org.apache.nifi.util.{TestRunner, TestRunners}
+import org.scalatest.{BeforeAndAfterEach, Matchers, WordSpec}
 
-class TestWordCountProcessor extends WordSpec with Matchers with BeforeAndAfterAll {
+import scala.collection.JavaConverters._
+
+class TestWordCountProcessor extends WordSpec with Matchers with BeforeAndAfterEach {
+
+  // for simplicity, we will stick to only these stop words.
+  private val stopWords: Seq[String] = Seq("of", "this", "the", "me", "myself", "his", "and", "a")
+
+  private val inputData: String = "Before you judge a man, walk a mile in his shoes." +
+    " After that who cares?... He’s a mile away and you’ve got his shoes!"
 
   "Word count processor" should {
+    "count occurance of stop words as well when 'skip stop words' property is false" in {
+      val testRunner: TestRunner = TestRunners.newTestRunner(new WordCountProcessor)
+      testRunner.setProperty(WordCountProcessor.skipStopWordsPropertyName, "false")
+
+      testRunner.enqueue(inputData)
+      testRunner.run()
+
+      val uniqueWordsInSentence = 20
+      testRunner.assertTransferCount(WordCountProcessor.successRelationName, uniqueWordsInSentence)
+      testRunner.assertTransferCount(WordCountProcessor.failureRelationName, 0)
+
+      val wordCounts: Seq[FlowFile] =
+        testRunner.getFlowFilesForRelationship(WordCountProcessor.successRelationName).asScala
+
+      val stopWordFlowFiles: Seq[FlowFile] =
+        wordCounts.filter(ff => stopWords.contains(ff.getAttribute("word").toLowerCase))
+
+      stopWordFlowFiles.isEmpty shouldBe false
+    }
+
     "not emmit any output flow file when input flow file is null" in {
+      val testRunner: TestRunner = TestRunners.newTestRunner(classOf[WordCountProcessor])
+      testRunner.setProperty(WordCountProcessor.skipStopWordsPropertyName, "false")
+      testRunner.run()
+      testRunner.assertTransferCount(WordCountProcessor.failureRelationName, 0)
+      testRunner.assertTransferCount(WordCountProcessor.successRelationName, 0)
     }
+
     "not emmit any output flow file when input flow file contents are empty" in {
+      val testRunner: TestRunner = TestRunners.newTestRunner(classOf[WordCountProcessor])
+      testRunner.setProperty(WordCountProcessor.skipStopWordsPropertyName, "false")
+      val inputData: String = ""
+      testRunner.enqueue(inputData)
+      testRunner.run()
+
+      testRunner.assertTransferCount(WordCountProcessor.failureRelationName, 0)
+      testRunner.assertTransferCount(WordCountProcessor.successRelationName, 0)
     }
-    "count occurance of stop words as well when 'process stop words' property is true" in {
-    }
-    "not count occurance of stop words as well when 'process stop words' property is false" in {
+
+    "not count occurance of stop words when 'skip stop words' property is true" in {
+      val testRunner: TestRunner = TestRunners.newTestRunner(classOf[WordCountProcessor])
+      testRunner.setProperty(WordCountProcessor.skipStopWordsPropertyName, "true")
+      testRunner.enqueue(inputData)
+      testRunner.run()
+
+      testRunner.assertTransferCount(WordCountProcessor.failureRelationName, 0)
+
+      val flowFilesInSuccessRelation: Seq[FlowFile] =
+        testRunner.getFlowFilesForRelationship(WordCountProcessor.successRelationName).asScala
+
+      val uniqueWordsInSentence = 20
+      val numberOfStopWordsInSentence = 3
+      val expectedFlowFiles: Int = uniqueWordsInSentence - numberOfStopWordsInSentence
+      flowFilesInSuccessRelation.length shouldEqual expectedFlowFiles
+
+      val stopWordFlowFiles: Seq[FlowFile] =
+        flowFilesInSuccessRelation.filter(ff => stopWords.contains(ff.getAttribute("word").toLowerCase))
+
+      stopWordFlowFiles.isEmpty shouldBe true
     }
   }
 }
